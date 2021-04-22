b0c1bf75b460aaf9b77f05096c4ab995be1acaa9
Vanshika4222 <67817404+Vanshika4222@users.noreply.github.com>
2021-04-22 17:16:30 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,52 +1,68 @@
 def call(script) {
    echo "Hello Vanshika welcome to MavenBuild shared library"
-def containers = [
-    containerTemplate(name: 'node', image: 'node', ttyEnabled: true),
-    containerTemplate(name: 'docker', image: 'docker', ttyEnabled: true)]
-podTemplate(label: maven-pod, containers: maven, serviceAccount: 'jenkins') {
+	   podTemplate(yaml: """\
+		apiVersion: v1
+		kind: Pod
+		metadata:
+		    name: maven-pod
+		  labels:
+		    some-label: maven-pod
+		spec:
+		  containers:
+		  - name: maven
+		    image: maven:alpine
+		    command:
+		    - cat
+		    tty: true
+		  - name: busybox
+		    image: busybox
+		    command:
+		    - cat
+		    tty: true
+		""".stripIndent()){
 
-	       node (maven-pod){
+       node (maven-pod){
+	  
+           stage("Tools initialization") {
+                   sh "mvn --version"
+                   sh "java -version"
+           }
+	       
+         stage("Checkout Code") {
+                   git branch: 'master',
+                       url: script.env.GIT_SOURCE_URL
+           }
 
-		 stage("Tools initialization") {
-			   sh "mvn --version"
-			   sh "java -version"
-		   }
-
-		 stage("Checkout Code") {
-			   git branch: 'master',
-			       url: script.env.GIT_SOURCE_URL
-		   }
-
-		 stage("Cleaning workspace") {
-			   sh "mvn clean"
-		   }
-		 stage("Compiling") {
-			   sh "mvn compile"
-		   }
-		   if(script.env.CODE_QUALITY == 'True'){
-			 stage("Code Quality Analysis") {
-			     echo 'Code Quality Analysis'
-			 }
-		   }
-		   if(script.env.UNIT_TESTING == 'True'){
-		 stage("Running Testcase") {
-			   sh "mvn test"
-		   }
-		   }
-		   if(script.env.CODE_COVERAGE == 'True')
-		{
-		 stage("Code Coverage") {
-		     echo 'Code Coverage'
+	   stage("Cleaning workspace") {
+                   sh "mvn clean"
+           }
+	   stage("Compiling") {
+                   sh "mvn compile"
+           }
+	   if(script.env.CODE_QUALITY == 'True'){
+		 stage("Code Quality Analysis") {
+		     echo 'Code Quality Analysis'
 		 }
-		}
-		 if(script.env.SECURITY_TESTING == 'True')
-		{
-		 stage("Security Testing") {
-		     echo 'Security Testing'
-		 }
-		}
-		   stage("Packaging Application") {
-			   sh "mvn package -DskipTests"
-		   }
-
-	       }}}
+	   }
+	   if(script.env.UNIT_TESTING == 'True'){
+           stage("Running Testcase") {
+                   sh "mvn test"
+           }
+	   }
+	   if(script.env.CODE_COVERAGE == 'True')
+        {
+         stage("Code Coverage") {
+             echo 'Code Coverage'
+         }
+        }
+         if(script.env.SECURITY_TESTING == 'True')
+        {
+         stage("Security Testing") {
+             echo 'Security Testing'
+         }
+        }
+           stage("Packaging Application") {
+                   sh "mvn package -DskipTests"
+           }
+	   
+       }}}
