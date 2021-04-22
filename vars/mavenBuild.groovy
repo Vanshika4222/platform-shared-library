5c6c26069a3e72935ece5c7b33beb0712f4abfcf
Vanshika4222 <67817404+Vanshika4222@users.noreply.github.com>
2021-04-22 17:11:16 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -3,48 +3,48 @@ def call(script) {
 	containerTemplate(name: 'maven', image: 'maven:alpine', ttyEnabled: true)
 	podTemplate(label: maven-pod, containers: maven, serviceAccount: 'jenkins') {
 
-       node (maven-pod){
-	  
-           stage("Tools initialization") {
-                   sh "mvn --version"
-                   sh "java -version"
-           }
-	       
-         stage("Checkout Code") {
-                   git branch: 'master',
-                       url: script.env.GIT_SOURCE_URL
-           }
+	       node (maven-pod){
 
-	   stage("Cleaning workspace") {
-                   sh "mvn clean"
-           }
-	   stage("Compiling") {
-                   sh "mvn compile"
-           }
-	   if(script.env.CODE_QUALITY == 'True'){
-		 stage("Code Quality Analysis") {
-		     echo 'Code Quality Analysis'
+		   stage("Tools initialization") {
+			   sh "mvn --version"
+			   sh "java -version"
+		   }
+
+		 stage("Checkout Code") {
+			   git branch: 'master',
+			       url: script.env.GIT_SOURCE_URL
+		   }
+
+		   stage("Cleaning workspace") {
+			   sh "mvn clean"
+		   }
+		   stage("Compiling") {
+			   sh "mvn compile"
+		   }
+		   if(script.env.CODE_QUALITY == 'True'){
+			 stage("Code Quality Analysis") {
+			     echo 'Code Quality Analysis'
+			 }
+		   }
+		   if(script.env.UNIT_TESTING == 'True'){
+		   stage("Running Testcase") {
+			   sh "mvn test"
+		   }
+		   }
+		   if(script.env.CODE_COVERAGE == 'True')
+		{
+		 stage("Code Coverage") {
+		     echo 'Code Coverage'
 		 }
-	   }
-	   if(script.env.UNIT_TESTING == 'True'){
-           stage("Running Testcase") {
-                   sh "mvn test"
-           }
-	   }
-	   if(script.env.CODE_COVERAGE == 'True')
-        {
-         stage("Code Coverage") {
-             echo 'Code Coverage'
-         }
-        }
-         if(script.env.SECURITY_TESTING == 'True')
-        {
-         stage("Security Testing") {
-             echo 'Security Testing'
-         }
-        }
-           stage("Packaging Application") {
-                   sh "mvn package -DskipTests"
-           }
-	   
-       }}
+		}
+		 if(script.env.SECURITY_TESTING == 'True')
+		{
+		 stage("Security Testing") {
+		     echo 'Security Testing'
+		 }
+		}
+		   stage("Packaging Application") {
+			   sh "mvn package -DskipTests"
+		   }
+
+	       }}}
